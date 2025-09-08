2d7f665cd1265fa9d9bc20ed79679025860086f3
OmarEssam01 <omaressam01@gmail.com>
2025-09-09 01:57:06 +0300

    edit backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -78,7 +78,7 @@ export async function login(req , res){
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true,
             sameSite: "None",
-            secure: process.env.NODE_ENVIRONMENT === "production"
+            secure: process.env.NODE_ENV === "production"
      })
      res.status(200).json({success: true , user})
     } catch (error) {
