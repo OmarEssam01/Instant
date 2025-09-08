fcd57460fc8a767b1249dbb5044bf4aa1ba5a5ae
OmarEssam01 <omaressam01@gmail.com>
2025-09-08 23:04:42 +0300

    edit axios

--- frontend/instant/src/lib/axios.js
+++ frontend/instant/src/lib/axios.js
@@ -1,8 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
 
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true // send cookies (JWT) with the request
-})
+  baseURL: BASE_URL,
+  withCredentials: true 
+});
